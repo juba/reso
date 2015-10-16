f3e9dc605e19d6f67f98bd4e0dc84999cd8be649
Julien Barnier <julien@nozav.org>
2015-10-16 11:33:38 +0200

    Add first tests

--- tests/testthat/test_ap_wp.R
+++ tests/testthat/test_ap_wp.R
@@ -1,3 +1,28 @@
 library(reso)
+library(igraph)
 context("AP, 1-AP and WP tests")
+
+## DATA -----------------------------------------------------------
+
+## Nolay graph
+nolay <- igraph::read_graph(file="graphs/nolay_comp1.txt",
+                            format="edgelist", directed=FALSE)
+V(nolay)$name <- as.character(c(30,31,77,32,110,34,90,70,103,109))
+
+## Social Networks 1994 graph
+sn94 <- igraph::read_graph(file="graphs/social_networks_1994_p148.txt",
+                           format="edgelist", n = 4, directed = TRUE)
+V(sn94)$name <- letters[1:4]
+
+
+
+## TESTS ----------------------------------------------------------
+
+test_that("ap_wp with 'weak' type is correct for nolay graph", {
+    expect_equal(ap_wp(nolay, type = "weak"),
+                 structure(list(ap = c("31", "32", "34"),
+                                ap_1 = c("31", "32", "34"),
+                                wp = c("77", "110", "103", "109", "90", "70")),
+                           .Names = c("ap", "ap_1", "wp")))
+})
 
--- tests/testthat/test_reso.R
+++ tests/testthat/test_reso.R
@@ -1,3 +1,34 @@
 library(reso)
+library(igraph)
 context("RESO algorithm test")
 
+
+## DATA -----------------------------------------------------------
+
+## Nolay graph
+nolay <- igraph::read_graph(file="graphs/nolay_comp1.txt",
+                            format="edgelist", directed=FALSE)
+V(nolay)$name <- as.character(c(30,31,77,32,110,34,90,70,103,109))
+
+## Gollac 1988 graph
+gollac <- igraph::read_graph(file="graphs/gollac_1988.txt",
+                             format="edgelist", n = 7, directed=TRUE)
+V(gollac)$name <- c("Inconnue", "Ouvriers", "Employés", "Interm", "Cadres", "Indep", "Agriculteurs")
+
+
+
+
+## TESTS -----------------------------------------------------------
+
+test_that("RESO for undirected gollac graph is correct", {
+  expect_equal(reso(gollac, type = "weak"),
+               c("WP_1", "WP_1", "1-AP_2", "CC_3", "CC_3", "CC_3", "CCsing_1"
+  ))
+})
+
+test_that("RESO with 'weak' type for directed nolay graph is correct", {
+  expect_equal(reso(nolay, type = "weak"),
+               c("CCsing_4", "1-AP_3", "WP_1", "1-AP_3", "WP_1", "WP_2", "WP_1",
+                 "WP_1", "WP_1", "WP_1")
+               )
+})
